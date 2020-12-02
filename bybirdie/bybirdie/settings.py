d6315cb00f5c63257d0c417ead34c0f549bd23c3
Kayliegh.Hill <kayliegh.hill@gmail.com>
2020-12-02 18:40:03 -0500

    user table, app created

--- bybirdie/bybirdie/settings.py
+++ bybirdie/bybirdie/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bb'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'bybirdie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bybirdie',
+        'USER': 'kayliegh',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
