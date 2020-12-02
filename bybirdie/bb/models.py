e70079fe4a3db5b9bebf128d1380d92a071d91ff
Kayliegh.Hill <kayliegh.hill@gmail.com>
2020-12-02 18:53:24 -0500

    added requirements.txt

--- bybirdie/bb/models.py
+++ bybirdie/bb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+#class Article(models.Model):
+#    pub_date = models.DateField()
+#    headline = models.CharField(max_length=200)
+#    content = models.TextField()
+ #   reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
